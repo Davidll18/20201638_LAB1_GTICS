3931c212cc853859cb3eb0f148c4bf1f1f85ba26
David Llanos <david.llanos@pucp.edu.pe>
2023-08-25 19:29:40 -0500

    first commit

---
+++ lab1_20201638/src/test/java/com/example/lab1_20201638/Lab120201638ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1_20201638;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab120201638ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
