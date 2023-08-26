67042dd260b7ad6ff0e8bc6955b00a8468400741
David Llanos <david.llanos@pucp.edu.pe>
2023-08-25 20:07:57 -0500

    Navbar listo

---
+++ lab1_20201638/src/main/java/com/example/lab1_20201638/controllers/PagPrincipal.java
@@ -0,0 +1,15 @@
+package com.example.lab1_20201638.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class PagPrincipal {
+
+    @RequestMapping(value = "/principal", method = RequestMethod.GET)
+    @ResponseBody
+    public String paginaPrincipal(){
+        return "principal";
+    }
+}
